609a85bb7f48160e40cb8818912a41140cdbffef
goodsmell <ck0302@naver.com>
2021-11-25 20:45:59 +0900

    Edit crawling code

--- src/scripts/coupang_search_crawling.py
+++ src/scripts/coupang_search_crawling.py
@@ -6,60 +6,60 @@ import time
 import json
 
 category_list = ['상의', '하의', '악세서리', '신발', '화장품']
+color_list = ['아이보리', '오렌지레드', '코랄핑크', '카멜', '머스타드', '오렌지', '버건디', '플럼', '블루그레이', '빨강', '블랙', '마젠타']
 
 base_url = 'https://www.coupang.com/np/search?component=&q={}&channel=user'
 cp_url = 'https://www.coupang.com/'
 
 result_list = {
-    "상의": [],
-    "하의": [],
-    "악세서리": [],
-    "신발": [],
-    "화장품": []
+    "상의": {},
+    "하의": {},
+    "악세서리": {},
+    "신발": {},
+    "화장품": {}
 }
 
 for category in category_list:
-    params = {
-        'q' : category,
-    }
+    for color in color_list:
+        result_list[category][color] = []
 
-    headers = {
-        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'    
-    }
+        params = {
+            'q' : category,
+        }
 
-    for page in range(1, 2):
-        url = base_url.format(category, page)
-        res = requests.get(url, headers=headers)
-        if res.status_code == 200:
-            soup = BeautifulSoup(res.text)
-            item_list = soup.select('ul#productList li')
-            for item in item_list:
-                try
-                    item_name = item.select_one('div.name').text.strip() #상품이름
-                    link = item.select_one('a').get('href')
-                    link = parse.urljoin(cp_url, link) #상품 링크
-                    price = item.select_one('strong.price-value').text.strip()
-                    price = ''.join(price.split(',')) #가격
+        headers = {
+            'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'    
+        }
+
+        for page in range(1, 2):
+            url = base_url.format(color + " " + category, page)
+            res = requests.get(url, headers=headers)
+            if res.status_code == 200:
+                soup = BeautifulSoup(res.text, "html.parser")
+                item_list = soup.select('ul#productList li')
+                for item in item_list:
+                    try:
+                        item_name = item.select_one('div.name').text.strip() #상품이름
+                        link = item.select_one('a').get('href')
+                        link = parse.urljoin(cp_url, link) #상품 링크
+                        price = item.select_one('strong.price-value').text.strip()
+                        price = ''.join(price.split(',')) #가격
+
+                        img_link = requests.get(link, headers=headers)
+                        img_link.raise_for_status()
+                        soup = BeautifulSoup(img_link.text, "html.parser") 
+                        img_link = "http:"+ soup.select_one('#repImageContainer > img')['src'] #상품 세부 이미지
+
+                        result_list[category][color].append({
+                            "itemName": item_name,
+                            "price": price,
+                            "link": link,
+                            "imgLink": img_link
+                        })             
+                    except:
+                        pass
 
 
-                    img_link = requests.get(link, headers=headers)
-                    img_link.raise_for_status()
-                    soup = BeautifulSoup(img_link.text) 
-                    img_link = "http:"+ soup.select_one('#repImageContainer > img')['src'] #상품 세부 이미지
-
-                    print(item_name)
-                    result_list[category].append({
-                        "itemName": item_name,
-                        "price": price,
-                        "link": link,
-                        "imgLink": img_link
-                    })             
-                except:
-                    pass
                    
-
-
-
-f = open("product.json", 'w')
-f.write(json.dumps(result_list))
-f.close()
+json_result = json.dumps(result_list)
+print(json_result, end="")
--- src/scripts/snap_musinsa.py
+++ src/scripts/snap_musinsa.py
@@ -10,21 +10,70 @@ headers = {
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'    
 }
 
-codi_data = []
+codi_data = {}
 
 driver = webdriver.Chrome()
 
-color = '베이지'
+color_list = ['흰색', '검정색', '회색', '갈색', '베이지색', '녹색', '파란색', '데님', '보라색', '노란색', '분홍색', '빨간색', '주황색', '은색', '금색', '기타']
 
-driver.get(f'https://magazine.musinsa.com/index.php?m=street&style=002&ordw=hit&_y=2021&color=5&swh=&stx={color}')
+for index in range(1, 17):
+    print(codi_data)
+    driver.get(f'https://magazine.musinsa.com/index.php?m=street&_y=2021&_mon=11%2C10%2C09&color={index}')
+    codi_data[color_list[index - 1]] = []
+    for index in range(0, 30):
+        try:
+            driver.find_elements(By.CLASS_NAME, 'articleImg')[index].click()
 
-images = driver.find_elements(By.CLASS_NAME, 'articleImg')
+            link = driver.current_url
+            res = requests.get(link, headers=headers)
+            res.raise_for_status()
+            soup = BeautifulSoup(res.text, "html.parser")
 
-for index in range(1, len(images) + 1):
-    driver.find_elements(By.CLASS_NAME, 'articleImg')[index].click()
-    driver.back()
+            listItem = soup.select_one(".styleItem-box")
+            a = listItem.select('a')
+            itemname = listItem.select('p')
+            
+            tbody = soup.select_one('tbody')
 
+            span = tbody.select('span')
+            name = str(span[1].text)
+            day = str(span[4].text)
+            area = str(span[9].text)
 
-f = open("style.json", 'w')
+            detail = []
+
+            url = driver.find_elements(By.CLASS_NAME, 'lbox')[0].get_attribute('href')
+
+            cnt = 0
+            for k in range(len(a)):
+                if(re.search('title(.+?)"', str(a[k]))):
+                    cnt = cnt + 1
+            
+            fir = -5
+            for i in range(cnt):
+                deitem = re.search('href="(.+?)"',str(a[fir+5]))
+                if deitem:
+                    deitem = deitem.group(1)
+                    detail.append({
+                        "title": itemname[i].text.strip(),
+                        "image": "https:" + deitem
+                    })
+                fir+=5
+
+            codi_data[color_list[index]].append({
+                "name": name,
+                "filmingDate": day,
+                "area": area,
+                "image": url,
+                "detail": detail
+            })
+
+            driver.back()
+        except:
+            break
+
+print(codi_data)
+
+f = open("../../data/style/style.json", 'w')
 f.write(json.dumps(codi_data))
 f.close()
