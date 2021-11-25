0a4182c459949c1194eab89bab71d3e85da80cbc
goodsmell <ck0302@naver.com>
2021-11-25 20:48:08 +0900

    Add data manager

--- src/module/data/product/productManager.ts
+++ src/module/data/product/productManager.ts
@@ -1,35 +1,42 @@
 import { spawn } from 'child_process'
 import { DataDirectoryPath } from '../../../types/module/data/data.types'
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from 'fs'
 
 export default class DataProductManager {
   static getProductPath() {
-    return `${DataDirectoryPath}/product`
+    const defaultPath = `${DataDirectoryPath}/product`
+
+    if (!existsSync(defaultPath)) {
+      mkdirSync(defaultPath, { recursive: true })
+    }
+
+    return defaultPath
   }
 
   static getProductList() {
-    
+    return JSON.parse(readFileSync(`${this.getProductPath()}/product.json`).toString())
   }
 
   static updateProductData() {
-    const result = spawn('./src/scripts/venv/Scripts/python', ['./src/scripts/scanConfig.py', ''])
+    const result = spawn('./src/scripts/venv/Scripts/python', ['./src/scripts/coupang_search_crawling.py'])
 
     result.stdout.on('data', function (data) {
-      if (data === undefined) return
-      console.log(JSON.parse(data))
+      writeFileSync('./data/product/product.json', data)
+    })
+
+    result.stderr.on('data', function (data) {
+      console.log(data.toString())
     })
   }
 
   static async run() {
+    this.updateProductData()
     setInterval(() => {
       this.updateProductData()
     }, 60 * 60 * 6 * 1000)
   }
 
-  static create() {
+  static create() {}
 
-  }
-
-  static delete() {
-    
-  }
+  static delete() {}
 }
--- src/module/data/style/styleManager.ts
+++ src/module/data/style/styleManager.ts
@@ -1,35 +1,34 @@
 import { spawn } from 'child_process'
 import { DataDirectoryPath } from '../../../types/module/data/data.types'
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from 'fs'
 
 export default class DataStyleManager {
   static getStylePath() {
-    return `${DataDirectoryPath}/style`
+    const defaultPath = `${DataDirectoryPath}/style`
+
+    if (!existsSync(defaultPath)) {
+      mkdirSync(defaultPath, { recursive: true })
+    }
+
+    return defaultPath
   }
 
   static getStyleList() {
-    
+    return JSON.parse(readFileSync(`${this.getStylePath()}/style.json`).toString())
   }
 
   static updateStyleData() {
-    const result = spawn('./src/scripts/venv/Scripts/python', ['./src/scripts/scanConfig.py', ''])
-
-    result.stdout.on('data', function (data) {
-      if (data === undefined) return
-      console.log(JSON.parse(data))
-    })
+    spawn('./src/scripts/venv/Scripts/python', ['./src/scripts/snap_musinsa.py'])
   }
 
   static async run() {
-    setInterval(() => {
-      this.updateStyleData()
-    }, 60 * 60 * 6 * 1000)
+    // this.updateStyleData()
+    // setInterval(() => {
+    //   this.updateStyleData()
+    // }, 60 * 60 * 6 * 1000)
   }
 
-  static create() {
+  static create() {}
 
-  }
-
-  static delete() {
-    
-  }
+  static delete() {}
 }
